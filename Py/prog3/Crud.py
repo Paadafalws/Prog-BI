254757ea32eba1e6751dcddaa5cc31a13d29225b
Paadafalws <99057639+Paadafalws@users.noreply.github.com>
2023-02-09 18:29:16 -0300

    1

--- Py/prog3/Crud.py
+++ Py/prog3/Crud.py
@@ -2,7 +2,18 @@ import PySimpleGUI as sg
 from random import randint
 import BD
 
-sg.theme('darkblue') 
+def front():
+    flayout= [
+        [sg.Text('Pardal Contabil')],
+        [sg.Text('   ')],
+        [sg.Button('Entrar')],[sg.Button('Sair')]
+    ]
+    
+    window=sg.Window ('PardalLog',flayout, size = (500,200),element_justification='center')
+    button = window.read()
+    if button == 'Entrar':
+        window.close()
+    sg.theme('darkblue') 
 layout = [  [sg.Text('PardalLog')],
             [sg.Text('Nome'), sg.InputText(' ',key='-NAME-')],
             [sg.Text('preço de venda '), sg.InputText(' ',key='-PVENDA-')],
@@ -10,8 +21,9 @@ layout = [  [sg.Text('PardalLog')],
             [sg.Button('Adicionar'), sg.Button('Sair')] ]
 
 # Create the Window
-window = sg.Window('crud', layout)
+window = sg.Window('crud', layout,size=(1000,500),element_justification='center' )
 # Event Loop to process "events" and get the "values" of the inputs
+
 while True:
     button,values = window.read()
     if button == 'Adicionar':
